e69d9f4970b6e8cf907117c2f8ec4bf0ac6f4bf8
RemorseUB <uniub@outlook.com>
2024-11-27 01:13:40 -0500

    f

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
-document.getElementById('login-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevents the form from submitting normally
-
-    // Define the correct password
-    const correctPassword = 'xeenoub';
-
-    // Get the entered password value
-    const password = document.getElementById('password').value;
-
-    // Check if the entered password is correct
-    if (password === correctPassword) {
-        // Redirect to another website if the password is correct
-        window.location.href = '/2.html';
-    } else {
-        // Show an alert if the password is incorrect
-        alert('Invalid password. Please try again.');
+// script.js
+function applyBackgroundColor() {
+    const savedColor = localStorage.getItem('bgColor');
+    if (savedColor) {
+        document.body.style.backgroundColor = savedColor;
     }
-});
+}
+
+function changeBackground(color) {
+    document.body.style.backgroundColor = color;
+    localStorage.setItem('bgColor', color);
+}
+
+document.addEventListener("DOMContentLoaded", applyBackgroundColor);
